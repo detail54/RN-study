3a1f12726aceb824369545e73d86d2aea9c4f3f2
daehyun <detail54@nate.com>
2023-01-03 17:43:18 +0900

    초기버전

--- App.js
+++ App.js
@@ -1,20 +1,113 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { API_KEY } from '@env'
+import * as Location from 'expo-location'
+import { useEffect, useState } from 'react'
+import {
+  ActivityIndicator,
+  Dimensions,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native'
+
+const { width: SCREEN_WIDTH } = Dimensions.get('window')
 
 export default function App() {
+  const [city, setCity] = useState('Loading....')
+  const [days, setDays] = useState([])
+  const [ok, setOk] = useState(true)
+
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync()
+    if (!granted) {
+      setOk(false)
+    }
+
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 })
+
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false },
+    )
+
+    setCity(location[0].city)
+
+    const response = await fetch(
+      `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`,
+    )
+    const json = await response.json()
+    setDays(json.daily)
+  }
+
+  useEffect(() => {
+    getWeather()
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        {days.length === 0 ? (
+          <View style={styles.day}>
+            <ActivityIndicator
+              color='white'
+              size='large'
+              style={{ marginTop: 10 }}
+            />
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View key={`day${index}`} style={styles.day}>
+              <Text style={styles.temp}>
+                {parseFloat(day.temp.day).toFixed(1)}
+              </Text>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
+      </ScrollView>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'tomato',
   },
-});
+  city: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: '500',
+  },
+  weather: {},
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: 'center',
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
+  },
+  tinyText: {
+    fontSize: 20,
+  },
+})
